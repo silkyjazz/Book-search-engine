c6deb01a2b50d49946cdcfcb6e3b536c0d370cbd
Jasmine Ulloa <ulloajazz6@gmail.com>
2023-03-09 19:23:54 -0800

    fixed typos

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const {ApolloServer} = require('apollo-server-express');
 const {authMiddleware} = require('./utils/auth');
@@ -10,7 +10,7 @@ const {typeDefs, resolvers} = require('./schemas');
 const app = express();
 const PORT = process.env.PORT || 3003;
 
-const server = new ApollosServer({
+const server = new ApolloServer({
   typeDefs, resolvers, context: authMiddleware
 });
 
